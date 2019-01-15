30a80546e669535bc9899f38f2f2eac1ec92a0b8
Sonja Jovanovic <sonja19833@gmail.com>
2019-01-15 21:11:19 +0100

    Uradjen zadatak brisanja vrednosti closes 7#

--- src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
+++ src/main/java/zadaci/Zadatak4BrisanjeVrednosti.java
@@ -1,18 +1,44 @@
 package zadaci;
 
+import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
+import model.Avion;
+import model.Roba;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Zadatak4BrisanjeVrednosti {
+
+    static Dao<Avion,Integer> avionDao;
+    static Dao<Roba,Integer> robaDao;
+
+
     public static void main(String[] args) {
 
         ConnectionSource connectionSource = null;
         try {
             // create our data-source for the database
             connectionSource = new JdbcConnectionSource("jdbc:sqlite:avionRoba.db");
+
+            robaDao = DaoManager.createDao(connectionSource, Roba.class);
+
+
+            List<Roba> robaZaBrisanje = robaDao.queryForEq(Roba.POLJE_NAZIV, "Voda");
+            Roba roba =robaZaBrisanje.get(0);
+            robaDao.delete(roba);
+
+            List<Roba> roba1 = robaDao.queryForAll();
+            for (Roba r : roba1)
+                System.out.println(r);
+
+
+
+
+
 
 
         }catch (Exception e) {
